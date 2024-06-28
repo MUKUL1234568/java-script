037fae35946a04e8f2485ce10987c5c5b400c109
mukul chauhan <mukulchauhan7407@gmail.com>
2024-06-28 11:13:59 +0530

    datatype basic

---
+++ 01base/datatype.js
@@ -0,0 +1,12 @@
+"use strict";   //newer version of java script 
+//null is a object
+// premitive datatype :-----  String ,Numbwr ,Boolean ,null undefind ,Symbol (call by value )
+
+// non premitive :-- Array ,object,Function 
+
+const id=Symbol('234');
+const is=Symbol("234");
+console.log(id==is);
+
+const bigint=12136655655556554n;
+console.log(bigint);
